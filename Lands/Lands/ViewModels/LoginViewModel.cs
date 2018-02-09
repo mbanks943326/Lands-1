b8181028c4e1e2bc2fd8772d34bdb11bbbee40b7
cayman01 <cayman01.filiales@gmail.com>
2018-02-08 19:21:04 -0500

    Login  continue...

---
+++ Lands/Lands/ViewModels/LoginViewModel.cs
@@ -0,0 +1,38 @@
+﻿namespace Lands.ViewModels
+{
+    public class LoginViewModel
+    {
+        #region Properties
+        public string Email
+        {
+            get;
+            set;
+        }
+
+        public string Password
+        {
+            get;
+            set;
+        }
+
+        public bool IsRunning
+        {
+            get;
+            set;
+        }
+
+        public bool IsRemembered
+        {
+            get;
+            set;
+        }
+        #endregion
+
+        #region Constructores
+        public LoginViewModel()
+        {
+            this.IsRemembered = true;
+        }
+        #endregion
+    }
+}
---
+++ Lands/Lands/ViewModels/MainViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace Lands.ViewModels
+{
+    public class MainViewModel
+    {
+        #region Views
+        public LoginViewModel Login
+        {
+            get;
+            set;
+        }
+        #endregion
+
+        #region Constructors
+        public MainViewModel()
+        {
+            this.Login = new LoginViewModel();
+        }
+        #endregion
+    }
+}
