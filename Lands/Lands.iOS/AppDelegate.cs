6a1f1c94197b3c14d34b34edd8279b3480abb105
cayman01 <cayman01.filiales@gmail.com>
2018-02-11 11:19:02 -0500

    Try to show images

--- Lands/Lands.iOS/AppDelegate.cs
+++ Lands/Lands.iOS/AppDelegate.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
-using Foundation;
-using UIKit;
-
-namespace Lands.iOS
+﻿namespace Lands.iOS
 {
+    using System;
+    using FFImageLoading;
+    using FFImageLoading.Forms.Touch;
+    using Foundation;
+    using UIKit;
+
     // The UIApplicationDelegate for the application. This class is responsible for launching the 
     // User Interface of the application, as well as listening (and optionally responding) to 
     // application events from iOS.
@@ -23,9 +22,41 @@ namespace Lands.iOS
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
             global::Xamarin.Forms.Forms.Init();
+
+            CachedImageRenderer.Init();
+
+            var config = new FFImageLoading.Config.Configuration()
+            {
+                VerboseLogging = false,
+                VerbosePerformanceLogging = false,
+                VerboseMemoryCacheLogging = false,
+                VerboseLoadingCancelledLogging = false,
+                Logger = new CustomLogger(),
+            };
+
+            ImageService.Instance.Initialize(config);
+
             LoadApplication(new App());
 
             return base.FinishedLaunching(app, options);
+        }
+
+        public class CustomLogger : FFImageLoading.Helpers.IMiniLogger
+        {
+            public void Debug(string message)
+            {
+                Console.WriteLine(message);
+            }
+
+            public void Error(string errorMessage)
+            {
+                Console.WriteLine(errorMessage);
+            }
+
+            public void Error(string errorMessage, Exception ex)
+            {
+                Error(errorMessage + System.Environment.NewLine + ex.ToString());
+            }
         }
     }
 }
