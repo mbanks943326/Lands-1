eb60f094ad3c8dd87f22432179e0045b1d5308b2
Cayman01 <cayman01.filiales@gmail.com>
2018-02-10 19:04:18 -0500

    Show countries list

--- Lands/Lands/ViewModels/LandsViewModel.cs
+++ Lands/Lands/ViewModels/LandsViewModel.cs
@@ -1,10 +1,12 @@
 ﻿namespace Lands.ViewModels
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Collections.ObjectModel;
+    using GalaSoft.MvvmLight.Command;
     using Models;
     using Services;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using System.Linq;
+    using System.Windows.Input;
     using Xamarin.Forms;
 
     public class LandsViewModel : BaseViewModel
@@ -15,13 +17,32 @@
 
         #region Attributes
         private ObservableCollection<Land> lands;
+        private List<Land> myLands;
+        private bool isRefreshing;
+        private string filter;
         #endregion
 
         #region Properties
+        public string Filter
+        {
+            get { return this.filter; }
+            set
+            {
+                this.Search();
+                SetValue(ref this.filter, value);
+            }
+        }
+
         public ObservableCollection<Land> Lands
         {
             get { return this.lands; }
             set { SetValue(ref this.lands, value); }
+        }
+
+        public bool IsRefreshing
+        {
+            get { return this.isRefreshing; }
+            set { SetValue(ref this.isRefreshing, value); }
         }
         #endregion
 
@@ -36,6 +57,20 @@
         #region Methods
         private async void LoadLands()
         {
+            this.IsRefreshing = true;
+            var connection = await this.apiService.CheckConnection();
+
+            if (!connection.IsSuccess)
+            {
+                this.IsRefreshing = false;
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    connection.Message,
+                    "Accept");
+                await Application.Current.MainPage.Navigation.PopToRootAsync();
+                return;
+            }
+
             var response = await this.apiService.GetList<Land>(
                 "http://restcountries.eu",
                 "/rest", 
@@ -43,6 +78,7 @@
 
             if (!response.IsSuccess)
             {
+                this.IsRefreshing = false;
                 await Application.Current.MainPage.DisplayAlert(
                     "Error", 
                     response.Message, 
@@ -50,8 +86,45 @@
                 return;
             }
 
-            var list = (List<Land>)response.Result;
-            this.Lands = new ObservableCollection<Land>(list);
+            this.myLands = (List<Land>)response.Result;
+            this.Lands = new ObservableCollection<Land>(this.myLands);
+            this.IsRefreshing = false;
+        }
+        #endregion
+
+        #region Commands
+        public ICommand RefreshCommand
+        {
+            get
+            {
+                return new RelayCommand(LoadLands);
+            }
+        }
+
+        public ICommand SearchCommand
+        {
+            get
+            {
+                return new RelayCommand(Search);
+            }
+        }
+
+        private void Search()
+        {
+            this.IsRefreshing = true;
+
+            if (string.IsNullOrEmpty(this.Filter))
+            {
+                this.Lands = new ObservableCollection<Land>(this.myLands);
+            }
+            else
+            {
+                this.Lands = new ObservableCollection<Land>(
+                    this.myLands.Where(l => l.Name.ToLower().
+                    Contains(this.Filter.ToLower())));
+            }
+
+            IsRefreshing = false;
         }
         #endregion
     }
