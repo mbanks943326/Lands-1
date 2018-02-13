13582c3cdb280e424375bfde6ef2325d16fd6496
cayman01 <cayman01.filiales@gmail.com>
2018-02-13 18:26:38 -0500

    Login Page

--- Lands/Lands/ViewModels/LoginViewModel.cs
+++ Lands/Lands/ViewModels/LoginViewModel.cs
@@ -1,29 +1,36 @@
 ﻿namespace Lands.ViewModels
 {
-    using GalaSoft.MvvmLight.Command;
     using System;
     using System.Windows.Input;
+    using GalaSoft.MvvmLight.Command;
     using Xamarin.Forms;
 
-    public class LoginViewModel
+    public class LoginViewModel : BaseViewModel
     {
+        #region Attributes
+        private string email;
+        private string password;
+        private bool isRunning;
+        private bool isEnabled;
+        #endregion
+
         #region Properties
         public string Email
         {
-            get;
-            set;
+            get { return this.email; }
+            set { this.SetValue(ref this.email, value); }
         }
 
         public string Password
         {
-            get;
-            set;
+            get { return this.password; }
+            set { this.SetValue(ref this.password, value); }
         }
 
         public bool IsRunning
         {
-            get;
-            set;
+            get { return this.isRunning; }
+            set { this.SetValue(ref this.isRunning, value); }
         }
 
         public bool IsRemembered
@@ -31,12 +38,19 @@
             get;
             set;
         }
+
+        public bool IsEnabled
+        {
+            get { return this.isEnabled; }
+            set { this.SetValue(ref this.isEnabled, value); }
+        }
         #endregion
 
         #region Constructores
         public LoginViewModel()
         {
             this.IsRemembered = true;
+            this.IsEnabled = true;
         }
         #endregion
 
@@ -68,6 +82,29 @@
                     "Accept");
                 return;
             }
+
+            this.IsRunning = true;
+            this.IsEnabled = false;
+
+            if (this.Email != "jzuluaga55@gmail.com" || this.Password != "1234")
+            {
+                this.IsRunning = false;
+                this.IsEnabled = true;
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "Email or password wrong.",
+                    "Accept");
+                this.Password = string.Empty;
+                return;
+            }
+
+            await Application.Current.MainPage.DisplayAlert(
+                "Error",
+                "Fuck yeahhh!!!!",
+                "Accept");
+
+            this.IsRunning = false;
+            this.IsEnabled = true;
         }
         #endregion
     }
