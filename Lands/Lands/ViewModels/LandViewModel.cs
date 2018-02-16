cb02193e17209c3f94be6cb6d49e72bc145bb954
cayman01 <cayman01.filiales@gmail.com>
2018-02-15 19:30:11 -0500

    Show country information

---
+++ Lands/Lands/ViewModels/LandViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace Lands.ViewModels
+{
+    using Models;
+
+    public class LandViewModel
+    {
+        #region Properties
+        public Land Land
+        {
+            get;
+            set;
+        }
+        #endregion
+
+        public LandViewModel(Land land)
+        {
+            this.Land = land;
+        }
+    }
+}
--- Lands/Lands/ViewModels/LandsViewModel.cs
+++ Lands/Lands/ViewModels/LandsViewModel.cs
@@ -1,7 +1,11 @@
 ﻿namespace Lands.ViewModels
 {
+    using System;
     using System.Collections.Generic;
     using System.Collections.ObjectModel;
+    using System.Linq;
+    using System.Windows.Input;
+    using GalaSoft.MvvmLight.Command;
     using Models;
     using Services;
     using Xamarin.Forms;
@@ -14,14 +18,32 @@
 
         #region Attributes
         private List<Land> landsList;
-        private ObservableCollection<Land> lands;
+        private ObservableCollection<LandItemViewModel> lands;
+        private bool isRefreshing;
+        private string filter;
         #endregion
 
         #region Properties
-        public ObservableCollection<Land> Lands
+        public ObservableCollection<LandItemViewModel> Lands
         {
             get { return this.lands; }
             set { this.SetValue(ref this.lands, value); }
+        }
+
+        public bool IsRefreshing
+        {
+            get { return this.isRefreshing; }
+            set { this.SetValue(ref this.isRefreshing, value); }
+        }
+
+        public string Filter
+        {
+            get { return this.filter; }
+            set 
+            { 
+                this.SetValue(ref this.filter, value);
+                this.Search();
+            }
         }
         #endregion
 
@@ -37,10 +59,12 @@
         #region Methods
         private async void LoadLands()
         {
+            this.IsRefreshing = true;
             var connection = await this.apiService.CheckConnection();
 
             if (!connection.IsSuccess)
             {
+                this.IsRefreshing = false;
                 await Application.Current.MainPage.DisplayAlert(
                     "Error", 
                     connection.Message, 
@@ -56,6 +80,7 @@
 
             if (!response.IsSuccess)
             {
+                this.IsRefreshing = false;
                 await Application.Current.MainPage.DisplayAlert(
                     "Error",
                     response.Message,
@@ -65,7 +90,79 @@
             }
 
             this.landsList = (List<Land>)response.Result;
-            var a = 1;
+            this.Lands = new ObservableCollection<LandItemViewModel>(
+                this.ToLandItemViewModel());
+            this.IsRefreshing = false;
+        }
+        #endregion
+
+        #region Commands
+        public ICommand RefreshCommand
+        {
+            get
+            {
+                return new RelayCommand(LoadLands);
+            }
+        }
+
+        public ICommand SearchCommand
+        {
+            get
+            {
+                return new RelayCommand(Search);
+            }
+        }
+
+        private void Search()
+        {
+            this.IsRefreshing = true;
+
+            if (string.IsNullOrEmpty(this.Filter))
+            {
+                this.Lands = new ObservableCollection<LandItemViewModel>(
+                    this.ToLandItemViewModel());
+            }
+            else
+            {
+                this.Lands = new ObservableCollection<LandItemViewModel>(
+                    this.ToLandItemViewModel().
+                    Where(l => l.Name.ToLower().Contains(this.Filter.ToLower())));
+            }
+
+            this.IsRefreshing = false;
+        }
+        #endregion
+
+        #region Methods
+        private List<LandItemViewModel> ToLandItemViewModel()
+        {
+            return this.landsList.Select(l => new LandItemViewModel
+            {
+                Alpha2Code = l.Alpha2Code,
+                Alpha3Code = l.Alpha3Code,
+                AltSpellings = l.AltSpellings,
+                Area = l.Area,
+                Borders = l.Borders,
+                CallingCodes = l.CallingCodes,
+                Capital = l.Capital,
+                Cioc = l.Cioc,
+                Currencies = l.Currencies,
+                Demonym = l.Demonym,
+                Flag = l.Flag,
+                Gini = l.Gini,
+                Languages = l.Languages,
+                Latlng = l.Latlng,
+                Name = l.Name,
+                NativeName = l.NativeName,
+                NumericCode = l.NumericCode,
+                Population = l.Population,
+                Region = l.Region,
+                RegionalBlocs = l.RegionalBlocs,
+                Subregion = l.Subregion,
+                Timezones = l.Timezones,
+                TopLevelDomain = l.TopLevelDomain,
+                Translations = l.Translations,
+            }).ToList();        
         }
         #endregion
     }
